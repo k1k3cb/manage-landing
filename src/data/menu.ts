61b08a6b03bb934ad2b201d528c0dbc6e9721e0f
Enrique Comesaña <enriquecbar@gmail.com>
2024-03-25 21:53:54 +0100

    Initial commit

---
+++ src/data/menu.ts
@@ -0,0 +1,22 @@
+export const MENU_ITEMS = [
+  {
+    name: 'Pricing',
+    path: '#'
+  },
+  {
+    name: 'Product',
+    path: '#'
+  },
+  {
+    name: 'About Us',
+    path: '#'
+  },
+  {
+    name: 'Careers',
+    path: '#'
+  },
+  {
+    name: 'Community',
+    path: '#'
+  }
+];
